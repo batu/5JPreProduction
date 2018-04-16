590ceeb319e6a6806de4b2efae0f22d26a112534
batu <ba921@nyu.edu>
2018-04-16 16:37:04 -0700

    very early AST visualization is there.

--- CodeSyntax/bundle.js
+++ CodeSyntax/bundle.js
@@ -48,72 +48,107 @@ function evaluateYAMLtoD3(){
 
 
 function evaluateASTtoD3(){
-	console.log("Yeah")
+	//console.log("In evaluateASTtoD3")
 	code_text = myCodeMirror.getValue();
 	var ast = Esprima.parse(code_text);
-	
-	// COPYING CODE FROM https://javascriptstore.com/2017/10/15/visualize-ast-javascript/
+
+	//Figure out what traverse does. 
+	traverse(ast, {
+	    pre: function (node) {
+	        if (node.body && !Array.isArray(node.body)) {
+	            node.body = [node.body];
+	        }
+	        node.children = node.body ? node.body : [];
+	    }
+	});  
+
+	console.log(ast) 
+	// https://javascriptstore.com/2017/10/15/visualize-ast-javascript/
 	// declares a tree layout and assigns the size
-	var margin = {top: 40, right: 90, bottom: 50, left: 90},
-            width = 660 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-	
-
-	var treemap = d3.tree()
-	   .size([width, height]);
-
-	// assigns the data to a hierarchy using parent-child relationships
-	var nodes = d3.hierarchy(ast);
-
-	// maps the node data to the tree layout
-	nodes = treemap(nodes);
-
-	var svg = d3.select("#graph").append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom),
-	 g = svg.append("g")
-            .attr("transform",
-                  "translate(" + margin.left + "," + margin.top + ")");
-
-	var link = svg.selectAll(".link")
-	.data(nodes.descendants().slice(1))
-	.enter().append("path")
-	.attr("class", "link")
-	.attr("d", function (d) {
-		  return "M" + d.x + "," + d.y
-		  + "C" + d.x + "," + (d.y + d.parent.y) / 2
-		  + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
-		  + " " + d.parent.x + "," + d.parent.y;
-	});
-
-	var node = g.selectAll(".node")
-			.data(nodes.descendants())
-			.enter().append("g")
-			.attr("class", function (d) {
-				return "node" +
-					(d.children ? " node--internal" : " node--leaf");
-			})
-			.attr("transform", function (d) {
-				return "translate(" + d.x + "," + d.y + ")";
-			});
-
-	node.append("circle")
-		.attr("r", 10);
-
-	node.append("text")
-	.attr({
-	   "dy": ".35em",
-	   "y": function (d) {
-				return d.children ? -20 : 20;
-		}})
-		.text(function (d) {
-		   return d.data.type;
-		});
+	renderTree(ast) 
 }
 
 
-// D3 Part
+ function renderTree(ast) {
+ 		container = "#graph"
 
+ 		// Remove old SVG
+ 		d3.select(container)
+ 			.select("svg")
+ 			.remove()
+
+
+ 		console.log(svg)
+        var treeData = ast;
+
+        // set the dimensions and margins of the diagram
+        var margin = {top: 40, right: 90, bottom: 50, left: 90},
+            width = 660 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        // declares a tree layout and assigns the size
+        var treemap = d3.tree()
+            .size([width, height]);
+
+        //  assigns the data to a hierarchy using parent-child relationships
+        var nodes = d3.hierarchy(treeData);
+
+        // maps the node data to the tree layout
+        nodes = treemap(nodes);
+
+        // append the svg obgect to the body of the page
+        // appends a 'group' element to 'svg'
+        // moves the 'group' element to the top left margin
+        var svg = d3.select(container).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom),
+            g = svg.append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+        // adds the links between the nodes
+        var link = g.selectAll(".link")
+            .data(nodes.descendants().slice(1))
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", function (d) {
+                return "M" + d.x + "," + d.y
+                    + "C" + d.x + "," + (d.y + d.parent.y) / 2
+                    + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
+                    + " " + d.parent.x + "," + d.parent.y;
+            });
+
+        // adds each node as a group
+        var node = g.selectAll(".node")
+            .data(nodes.descendants())
+            .enter().append("g")
+            .attr("class", function (d) {
+                return "node" +
+                    (d.children ? " node--internal" : " node--leaf");
+            })
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+
+        // adds the circle to the node
+        node.append("circle")
+            .attr("r", 10);
+
+        // adds the text to the node
+        node.append("text")
+            .attr("dy", ".35em")
+            .attr("y", function (d) {
+                return d.children ? -20 : 20;
+            })
+            .style("text-anchor", "middle")
+            .text(function (d) {
+                return d.data.type;
+            });
+}
+
+
+
+// D3 Part
 function circles(){
 	var vis = d3.select("#graph")
 	.append("svg")
