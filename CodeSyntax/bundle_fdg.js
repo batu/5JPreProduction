0233381cdea92e8bf29b95ffee99b12e51d08703
batu <ba921@nyu.edu>
2018-05-14 14:29:58 -0700

    Added the FDG component

--- CodeSyntax/bundle_fdg.js
+++ CodeSyntax/bundle_fdg.js
@@ -39,27 +39,23 @@ myCodeMirror.on("change", function(){
 function evaluateASTtoD3(){
 
 	code_text = myCodeMirror.getValue();
-	var ast;
 	var parseError = false;
+	var graph;
 	try {
-		ast = Esprima.parse(code_text);
+		var graph = JSON.parse(code_text)
 	} catch (e) {
 		parseError = true;
-		ast = {'errorMessage': e.message, 'errorObject': e};
+		graph = {'errorMessage': e.message, 'errorObject': e};
 	}
-
 	// https://javascriptstore.com/2017/10/15/visualize-ast-javascript/
 	// declares a tree layout and assigns the size
-
-	if (!parseError && ast["body"].length != 0) {
-		drawASTwithD3(ast);
+	if (!parseError && graph.length != 0) {
+		drawForceDirected(graph)
 	}else{
-		console.log(ast["errorMessage"])
-		drawError(ast);
+		console.log(graph["errorMessage"])
+		drawError(graph);
 	}
-
 }
-
 function drawError(ast){
 
 	var container = "#drawArea"
@@ -101,226 +97,90 @@ function drawError(ast){
 
 }
 
-function drawASTwithD3(ast) {
+function drawForceDirected(graph){
 
-	function drawAddTextToD3Node(node, current_d3_node) {
-		var renderableTitles = ["IfStatement", "WhileStatement"]
-		var typesToSkip = ["VariableDeclaration", "BinaryExpression", "Identifier", "ExpressionStatement"]
+	var container = "#drawArea"
 
-		switch (node.type) {
-			case 'CallExpression':
-				current_d3_node["text"] = "Called " + node.callee.name;
-				break;
-			case 'VariableDeclarator':
-				current_d3_node["text"] = node.id.name;
-				break;
-			case 'IfStatement':
-				current_d3_node["text"] = "If";
-				break;
-			case 'WhileStatement':
-				current_d3_node["text"] = "While";
-				break;
-			case 'Literal':
-				current_d3_node["text"] = node["value"];
-				break;
-			case 'Program':
-				current_d3_node["text"] = "Program"
-				break;
-			case 'ExpressionStatement':
-				current_d3_node["text"] = "Expression"
-				break;
-			case 'FunctionDeclaration':
-				current_d3_node["text"] = "Declare " + node.id.name;
-				break;
-			default:
-				for (var prop in node) {
-					if (node.hasOwnProperty(prop) && prop != 'type' &&
-							typeof node[prop] != 'object') {
-						current_d3_node["text"] = node[prop];
-					}
-				}
-				break;
-		}
-	}
-
-	function enumerateChildrenForD3Tree(ast, current_node) {
-
-		// Find children of this node
-		var children = [];
-		// The order of these matters
-		// eg in a while statement the test must come before the body
-		var typesToSkip = ["VariableDeclaration", "BinaryExpression", "Identifier", "ExpressionStatement", "AssignmentExpression"]
-
-		var possibleChildProperties = ['test',
-																	 'body',
-																	 'consequent',
-																	 'alternate',
-																	 'init',
-																	 'declarations',
-																	 'left',
-																	 'right',
-																	 'expression',
-																	 'argument',
-																	 'arguments'];
-
-		for (var i = 0; i < possibleChildProperties.length; i++) {
-			if (ast.hasOwnProperty(possibleChildProperties[i]) &&	ast[possibleChildProperties[i]] !== null){
-				children = children.concat(ast[possibleChildProperties[i]]);
-
-			}
-		}
-
-		for(var j = 0; j < children.length; j++){
-			current_node["children"].push({"parent":current_node,
-																		 "text":"",
-																	 	 "children":[]})
-		}
-		// console.log(children)
-		// console.log(current_node["children"])
-
-		// Draw this node
-		drawAddTextToD3Node(ast, current_node);
-
-		for (var i = 0; i < current_node["children"].length; i++) {
-			enumerateChildrenForD3Tree(children[i], current_node["children"][i]);
-		}
-	}
-
-	// Send the root node in for enumeration
-
-	container = "#drawArea"
-
-	var scale = 1.5;
-	// set the dimensions and margins of the diagram
 	d3.select(container)
 		.select("svg")
 		.remove()
 
-  var tree = {"parent":null,
-							"children":[],
-							"text":""}
-
-	enumerateChildrenForD3Tree(ast, tree);
-	tree = removeEmptyNodes(tree)
-	drawD3fromTree(tree)
-}
-
-function removeEmptyNodes(node){
-	if(!node["text"]){
-		for (var i = 0; i < node["children"].length; i++) {
-			node["children"][i]["parent"] = node["parent"]
-			node["parent"]["children"].push(node["children"][i])
-		}
-
-		var index = node["parent"]["children"].indexOf(node);
-		if (index > -1) {
-			node["parent"]["children"].splice(index, 1);
-			console.log(node["parent"]["children"])
-		}
-	}
-
-	if(node["children"].length == 0){
-		return node
-	} else {
-		for (var i = 0; i < node["children"].length; i++) {
-			removeEmptyNodes(node["children"][i])
-		}
-	}
-	return node
-}
-
-var svg_witdh;
-var svg_height;
-function drawD3fromTree(tree){
-	var root = d3.hierarchy(tree)
-
-	var radius = 45
-	var y_margin = radius
-	svg_witdh = root.children.length * 400;
-	svg_height = root.height * 125 + radius
 	var svg = d3.select(container).append("svg");
+
+	svg_witdh = 640;
+	svg_height = 640;
+
 	svg.attr("width", svg_witdh);
 	svg.attr("height", svg_height);
 
-
-	var treeLayout = d3.tree();
-
-	treeLayout.size([svg_witdh, svg_height - radius * 2]);
-	treeLayout(root);
-
 	var simulation = d3.forceSimulation()
-		.force("link", d3.forceLink().id(function(d) { return d.id; }))
-		.force("charge", d3.forceManyBody())
-		.force("center", d3.forceCenter(svg_witdh / 2, svg_height / 2));
+	    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+	    .force("charge", d3.forceManyBody())
+	    .force("center", d3.forceCenter(svg_witdh / 2, svg_height / 2));
+
+  var link = svg.append("g")
+      .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line")
+      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter().append("circle")
+      .attr("r", 10)
+      .attr("fill", function(d) {
+				return d.color;
+			})
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
+
+  node.append("title")
+      .text(function(d) { return d.id; });
+
+  simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(graph.links);
+
+	function dragstarted(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+	  d.fx = d.x;
+	  d.fy = d.y;
+	}
+
+	function dragged(d) {
+	  d.fx = d3.event.x;
+	  d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+	  if (!d3.event.active) simulation.alphaTarget(0);
+	  d.fx = null;
+	  d.fy = null;
+	}
 
 
-	simulation
-				.nodes(root.descendants())
-				.on("tick", ticked);
+  function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
-	simulation.force("link")
-				.links(root.links());
-
-	function ticked() {
-		// Links
-		var d3links = d3.select('svg')
-			.selectAll('line.link')
-			.data(root.links())
-			.enter()
-			.append('line')
-			.classed('link', true)
-			.attr('x1', function(d) {return d.source.x;})
-			.attr('y1', function(d) {return (d.source.y + y_margin);})
-			.attr('x2', function(d) {return d.target.x;})
-			.attr('y2', function(d) {return (d.target.y + y_margin)})
-			.attr("stroke", "black")
-			.attr("store-width", 1)
-
-	 var d3nodes = d3.select('svg')
-			.selectAll('circle.node')
-			.data(root.descendants())
-			.enter()
-			.append('circle')
-				.classed('node', true)
-				.attr('cx', function(d) {return d.x;})
-				.attr('cy', function(d) {return (d.y + y_margin)})
-				.attr('r', radius)
-				.attr("fill", "#3AA")
-
-		 var d3text = d3.select('svg')
-			.selectAll('text.text')
-			.data(root.descendants())
-			.enter()
-			.append("text")
-				.attr("x", function(d) {return d.x;})
-				.attr("y", function(d) {return (d.y + y_margin)})
-				.attr("text-anchor", "middle")
-				.text(function(d) {return d["data"]["text"]})
-				.attr("font-family", nodeFont["font-family"])
-				.attr('fill', nodeFont["fill"])
-				.attr("font-size", nodeFont["font-sis"])
-
-		}
-
-
+    node
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
 }
 
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
-}
 
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-}
-
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-}
 
 
 d3.select('#saveButton').on('click', function(){
